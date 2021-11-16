65ac501057f0a9f6dc903a8c78a9dbad651bc8f4
email <abe.solis2015@gmail.com>
2021-11-15 17:59:08 -0800

    adding an array of questions and answers

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const questions = ['What should appear at the very end of your JavaScript?', 'What are variables used for in JavaScript Programs?', ' Inside which HTML element do we put the JavaScript?', '_________ is a wrapped Java array, accessed from within JavaScript code.', 'What is the correct JavaScript syntax to write "Hello World" ?']
+
+const answers = ['The </script>', 'Storing numbers, dates, or other values', '<script>', 'JavaArray', 'document.write("Hello World")']
+
+
+
+
+
+
+// Start-NewRound
+
+document.getElementById('startGame').addEventListener('click', () => newGame())
+
